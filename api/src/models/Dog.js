e42c63a75e447d3f12d5753803ddda91cf1feb40
Andres Garcia <andresfgarciac96@gmail.com>
2023-03-25 20:08:08 -0500

    Termine Base de Datos

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,10 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define("dog", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span: {
       type: DataTypes.STRING,
       allowNull: false,
     },
